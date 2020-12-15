edee49558e0bb0cdb5df39932487e7d48532b554
andriu-ru <svjazinet@yandex.ru>
2020-12-15 13:37:22 +0300

    start. no code.

---
+++ phone-security-device/main.c
@@ -0,0 +1,18 @@
+/*
+ * phone-security-device.c
+ *
+ * Created: 15.12.2020 13:33:54
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
